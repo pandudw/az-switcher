b27b053ccca5c8ab738ba4dd0a0c8a33d50e7dc8
Nasrullah Pandu <103873184+pandudw@users.noreply.github.com>
2023-08-08 10:25:50 +0700

    Add main functions switcher

--- az-switcher.sh
+++ az-switcher.sh
@@ -19,3 +19,37 @@ switch_tenant() {
     echo "Berhasil masuk ulang dengan tenant: $tenant_id"
 }
 
+# Main loop
+while true; do
+    echo "wELCOME TO AZURE SWITCHER!"
+    echo "Choose Actions:"
+    echo "1. Display subscription list "
+    echo "2. Select a subscription"
+    echo "3. Switch to another tenant"
+    echo "4. Exit"
+    read choice
+
+    case $choice in
+        1)
+            list_subscriptions
+            ;;
+        2)
+            list_subscriptions
+            echo "Enter the ID of the subscription you want to select:"
+            read sub_id
+            select_subscription "$sub_id"
+            ;;
+        3)
+            echo "Enter the tenant ID you want to use:"
+            read tenant_id
+            switch_tenant "$tenant_id"
+            ;;
+        4)
+            echo "Thank You,!"
+            break
+            ;;
+        *)
+            echo "Invalid choice."
+            ;;
+    esac
+done
